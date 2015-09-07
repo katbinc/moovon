43406a641708f3fbbd85bd8a4a895d3e98b28b88
Ivan Antsimonau <iantsimonau@scnsoft.com>
2015-09-07 10:02:00 +0300

    sample app updated;

--- app/src/main/java/by/katbinc/moovon/app/activity/MainActivity.java
+++ app/src/main/java/by/katbinc/moovon/app/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package by.katbinc.moovon.app.activity;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import by.katbinc.moovon.app.R;
 import by.katbinc.moovon.fragment.StreamListFragment;
@@ -12,11 +14,16 @@ import by.katbinc.moovon.fragment.StreamListFragment;
  * Created on 31.08.15.
  * (c)
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Your toolbar");
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Fragment fragment = new StreamListFragment();
 
@@ -24,5 +31,24 @@ public class MainActivity extends Activity {
         fTransaction.add(R.id.mainContent, fragment);
         fTransaction.commit();
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStackImmediate();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
     }
 }
