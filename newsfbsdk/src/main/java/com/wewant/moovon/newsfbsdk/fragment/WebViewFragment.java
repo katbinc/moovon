ea413d3e36f3fbe3f5daae197c63112804f5bb63
Ivan Antsimonau <iantsimonau@scnsoft.com>
2015-09-14 10:32:46 +0300

    webview improved

--- newsfbsdk/src/main/java/com/wewant/moovon/newsfbsdk/fragment/WebViewFragment.java
+++ newsfbsdk/src/main/java/com/wewant/moovon/newsfbsdk/fragment/WebViewFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.wewant.moovon.newsfbsdk.R;
 
@@ -14,6 +17,8 @@ public class WebViewFragment extends Fragment {
     public static final String TAG = WebViewFragment.class.getSimpleName();
 
     private static final String KEY_URL = "url";
+
+    private ProgressBar preloader;
 
     public static WebViewFragment newInstance(String url) {
         WebViewFragment result = new WebViewFragment();
@@ -28,12 +33,42 @@ public class WebViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_webview, container, false);
 
+        preloader = (ProgressBar) view.findViewById(R.id.preloader);
+
         String url = getArguments().getString(KEY_URL);
         WebView webview = (WebView) view.findViewById(R.id.webview);
-
-//        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new SdkWebViewClient());
+        webview.setWebChromeClient(new SdkWebChromeClient());
+        webview.getSettings().setLoadWithOverviewMode(true);
+        webview.getSettings().setBuiltInZoomControls(true);
+        webview.getSettings().setSupportZoom(true);
+        webview.getSettings().setUseWideViewPort(true);
+        webview.getSettings().setJavaScriptEnabled(true);
         webview.loadUrl(url);
         return view;
+    }
+
+    public class SdkWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
+    }
+
+
+    public class SdkWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int progress) {
+            if (progress < 100) {
+                preloader.setVisibility(View.VISIBLE);
+                preloader.setProgress(progress);
+            }
+
+            if (progress == 100) {
+                preloader.setVisibility(View.GONE);
+            }
+        }
     }
 
 }
