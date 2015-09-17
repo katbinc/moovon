c7366773468d5f23b0a973f865430b9e8cb43579
Ivan Antsimonau <iantsimonau@scnsoft.com>
2015-09-17 17:40:16 +0300

    fixed animation popup issue

--- newsfbsdk/src/main/java/com/wewant/moovon/newsfbsdk/fragment/NewsListFragment.java
+++ newsfbsdk/src/main/java/com/wewant/moovon/newsfbsdk/fragment/NewsListFragment.java
@@ -10,7 +10,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
